595c79cafb8a14abe64620c3860c4e183967669f
Roman <kaboom.ua@gmail.com>
2016-05-30 21:23:21 -0700

    Update subscribeToArticleStore.js

--- src/decorators/subscribeToArticleStore.js
+++ src/decorators/subscribeToArticleStore.js
@@ -2,6 +2,7 @@ import React from 'react'
 import BasicSubscribeTo from './SubscribeToStore'
 import { articleStore } from '../stores'
 
+//При разработке HOC надо делать их общими, что б один использовать везде, а не в создавать новые для каждого случая - тогда теряется весь смысл
 export default (CustomComponent) => {
     return class DecoratedComponent extends BasicSubscribeTo {
         constructor() {
